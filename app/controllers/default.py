c31549f0221cfae01bac65d80f88096df7f941c7
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-07 11:58:25 -0300

    lascou-se

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,9 +1,8 @@
 from app import app
 from flask import Flask, request, url_for, render_template
 from requests import get, post
-from app.controllers import FireWar
 from colorama import Fore, Style
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    
+    return render_template('index.html')
