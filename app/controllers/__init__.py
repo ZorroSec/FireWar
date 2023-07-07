4fdcaf186673eab674c3e5179304ea826411609c
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-07 09:13:31 -0300

    change files

---
+++ app/controllers/__init__.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import request,  url_for, redirect, render_template
+from requests import get, post
+from colorama import Fore, Style
+
+class FireWar:
+    def __init__(self, name, password) -> None:
+        self.name = name
+        self.password = password
+    
+    def home(self):
+        if self.name == 'Zezo':
+            render_template('')
---
+++ app/controllers/default.py
@@ -0,0 +1,4 @@
+from app import app
+from flask import Flask, request, url_for
+from requests import get, post
+
