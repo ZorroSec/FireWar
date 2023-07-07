53e1d5df02bd1ec12c89d902555cfc1b72951334
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-07 10:39:43 -0300

    removendo o login do run e passando para o defult.py

--- run.py
+++ run.py
@@ -2,11 +2,7 @@ from app import app
 from flask import Flask
 from colorama import Fore
 
-name = input(f'{Fore.LIGHTBLACK_EX}[*] {Fore.LIGHTBLUE_EX}=+> {Fore.RED}')
-password = input(f'{Fore.LIGHTBLACK_EX}[*] {Fore.LIGHTBLUE_EX}=+> {Fore.RED}')
-
-if password == "Zezao":
-    if __name__ == '__main__':
-        app.run(
-            debug=True
-        )
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
