9bde3af074b15964900cd32fe464408ec925d583
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-07-07 10:33:25 -0300

    Importando o modulo colorama e implantando o servidor de usuarios no default.py

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,3 +2,10 @@ from app import app
 from flask import Flask, request, url_for, render_template
 from requests import get, post
 from app.controllers import FireWar
+from colorama import Fore, Style
+
+name = input(f'{Fore.RE}')
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    RE
